17c4bf3d4b309ab33a3e904f46430ef1339f53ec
Angel Muelas <angel.muelas@denizen.io>
2017-09-14 17:13:16 +0200

    feat: aditional files

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Opening up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
